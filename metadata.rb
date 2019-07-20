716e4d871a9b7a06768adeb9b6eb72623ef88cc3
devoptimist <sbrown@chef.io>
2019-07-21 00:10:05 +0200

    test data added

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures test_3'
 long_description 'Installs/Configures test_3'
-version '0.1.0'
+version '0.1.2'
+
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
